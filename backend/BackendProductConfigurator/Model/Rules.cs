d868a586b47e638522698081565d8fddbab8301c
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-01-15 13:45:21 +0100

    Update 220115 - 2
    Gitignore added

--- backend/BackendProductConfigurator/Model/Rules.cs
+++ backend/BackendProductConfigurator/Model/Rules.cs
@@ -5,11 +5,11 @@ namespace Model
     public class Rules
     {
         public float BasePrice { get; set; }
-        public List<string> DefaultOptions { get; set; }
-        public Dictionary<string, List<string>> ReplacementGroups { get; set; }
-        public Dictionary<string, List<string>> Requirements { get; set; }
-        public Dictionary<string, List<string>> Incompatibilities { get; set; }
-        public Dictionary<string, List<string>> GroupRequirements { get; set; }
-        public Dictionary<string, float> PriceList { get; set; }
+        public List<string> DefaultOptions { get; set; } = new List<string>();
+        public Dictionary<string, List<string>> ReplacementGroups { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> Requirements { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> Incompatibilities { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> GroupRequirements { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, float> PriceList { get; set; } = new Dictionary<string, float>();
     }
 }
