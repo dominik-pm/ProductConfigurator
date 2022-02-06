fdbf51a07779ee3df5422b2e7f475e787e8c4fce
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-06 11:51:31 +0100

    Added "Rules" and added connection to "RulesExtended"

---
+++ backend/BackendProductConfigurator/Model/Rules.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Rules
+    {
+        public float BasePrice { get; set; }
+        public string DefaultModel { get; set; }
+        public List<ModelType> Models { get; set; } = new List<ModelType>();
+        public Dictionary<string, List<string>> Requirements { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> Incompatibilities { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> GroupRequirements { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, float> PriceList { get; set; } = new Dictionary<string, float>();
+
+    }
+}
--- backend/BackendProductConfigurator/Model/RulesExtended.cs
+++ backend/BackendProductConfigurator/Model/RulesExtended.cs
@@ -2,15 +2,8 @@
 
 namespace Model
 {
-    public class RulesExtended
+    public class RulesExtended : Rules
     {
-        public float BasePrice { get; set; }
-        public string DefaultModel { get; set; }
-        public List<ModelType> Models { get; set; } = new List<ModelType>();
         public Dictionary<string, List<string>> ReplacementGroups { get; set; } = new Dictionary<string, List<string>>();
-        public Dictionary<string, List<string>> Requirements { get; set; } = new Dictionary<string, List<string>>();
-        public Dictionary<string, List<string>> Incompatibilities { get; set; } = new Dictionary<string, List<string>>();
-        public Dictionary<string, List<string>> GroupRequirements { get; set; } = new Dictionary<string, List<string>>();
-        public Dictionary<string, float> PriceList { get; set; } = new Dictionary<string, float>();
     }
 }
