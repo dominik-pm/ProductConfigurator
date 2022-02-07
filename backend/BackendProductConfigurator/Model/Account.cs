b6924e4f6ea4fadb6eaee1b037a7f32c87044638
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-07 13:41:25 +0100

    Added User-comparing method

--- backend/BackendProductConfigurator/Model/Account.cs
+++ backend/BackendProductConfigurator/Model/Account.cs
@@ -11,5 +11,12 @@ namespace Model
     {
         public string UserName { get; set; }
         public string UserEmail { get; set; }
+        public bool IsSameUser(Account temp)
+        {
+            if(this.UserName == temp.UserName)
+                if(this.UserEmail == temp.UserEmail)
+                    return true;
+            return false;
+        }
     }
 }
