be4ff0efedd70f6e6f9f80a77af5796483336ced
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-09 08:43:17 +0100

    Added own class for configuration-Controller

---
+++ backend/BackendProductConfigurator/BackendProductConfigurator/Controllers/ConfigurationController.cs
@@ -0,0 +1,53 @@
+﻿using BackendProductConfigurator.MediaProducers;
+using BackendProductConfigurator.Validation;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Model.Interfaces;
+using Model.Wrapper;
+
+namespace BackendProductConfigurator.Controllers
+{
+    [Route("configuration")]
+    public partial class ConfigurationController : AController<Configurator, string>
+    {
+        public ConfigurationController() : base()
+        {
+            entities = AValuesClass.Configurators;
+        }
+
+        private void AddConfigurator(Configurator value)
+        {
+            entities[GetAccLang(Request)].Add(value);
+            AValuesClass.ConfiguratorsSlim[GetAccLang(Request)].Add(value);
+        }
+
+        // GET api/<Controller>/5
+        [HttpGet("{id}")]
+        public override Configurator Get(string id)
+        {
+            Response.Headers.AcceptLanguage = Request.Headers.AcceptLanguage;
+            return entities[GetAccLang(Request)].Find(entity => entity.ConfigId.Equals(id));
+        }
+
+        // POST api/<Controller>
+        [HttpPost]
+        public void Post([FromBody] ConfiguratorPost value)
+        {
+            Configurator configurator = AValuesClass.GenerateConfigurator(value, GetAccLang(Request));
+
+            EValidationResult validationResult = ValidationMethods.ValidateConfigurator(configurator);
+            if (validationResult == EValidationResult.ValidationPassed)
+            {
+                AddConfigurator(configurator);
+                AValuesClass.PostValue<Configurator>(configurator, GetAccLang(Request));
+            }
+        }
+        public override void Delete(string id)
+        {
+            Response.Headers.AcceptLanguage = Request.Headers.AcceptLanguage;
+            entities[GetAccLang(Request)].Remove(entities[GetAccLang(Request)].Find(entity => (entity as IConfigId).ConfigId.Equals(id)));
+            AValuesClass.ConfiguratorsSlim[GetAccLang(Request)].Remove(AValuesClass.ConfiguratorsSlim[GetAccLang(Request)].Find(entity => (entity as IConfigId).ConfigId.Equals(id)));
+            AValuesClass.DeleteValue<ConfigurationDeleteWrapper>(GetAccLang(Request), new ConfigurationDeleteWrapper() { ConfigId = id });
+        }
+    }
+}
