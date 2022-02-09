76aee35c30ff44edf70adcada5246488c1d7f312
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-09 08:47:28 +0100

    Added own class for ConfiguredProductsController

--- backend/BackendProductConfigurator/BackendProductConfigurator/Controllers/AController.cs
+++ backend/BackendProductConfigurator/BackendProductConfigurator/Controllers/AController.cs
@@ -73,60 +73,7 @@ namespace BackendProductConfigurator.Controllers
     }
 
     
-    [Route("configuredProducts")]
-    public partial class ConfiguredProductsController : AController<ConfiguredProduct, string>
-    {
-        public ConfiguredProductsController() : base()
-        {
-            entities = AValuesClass.ConfiguredProducts;
-        }
-
-        // POST: /products
-        [Route("/products/{configId}")]
-        [HttpPost]
-        public void Post([FromBody] ConfiguredProduct value, string configId)
-        {
-            EValidationResult validationResult;
-            Configurator configurator = AValuesClass.Configurators[GetAccLang(Request)].Find(config => config.ConfigId == configId);
-            validationResult = ValidationMethods.ValidateConfiguration(value, configurator);
-            if (validationResult == EValidationResult.ValidationPassed)
-            {
-                validationResult = ValidationMethods.ValidatePrice(value, configurator.Rules);
-            }
-            if (validationResult == EValidationResult.ValidationPassed)
-            {
-                validationResult = ValidationMethods.ValidateSelectedModel(value, configurator);
-            }
-            //new Thread(() =>
-            //{
-            //    EmailProducer.SendEmail(value, validationResult);
-            //}).Start();
-            //new Thread(() =>
-            //{
-            //    validationResult = EValidationResult.ValidationPassed;
-            //    if(validationResult == EValidationResult.ValidationPassed)
-            //        PdfProducer.GeneratePDF(value, configId, Request);
-            //}).Start();
-            entities[GetAccLang(Request)].Add(value);
-
-            //Account account = AValuesClass.FillAccountFromToken(Request.Headers["Authorization"]);
-
-            SavedConfigsController scc = new SavedConfigsController();
-            Account tempAccount = new Account() { UserName = "testUser", UserEmail = "test@user.com" };
-            ProductSaveExtended temp = new ProductSaveExtended()
-            {
-                Status = EStatus.ordered.ToString(),
-                Date = DateTime.Now,
-                ConfigId = configurator.ConfigId,
-                Name = configurator.Name,
-                Description = configurator.Description,
-                Options = value.Options,
-                SavedName = value.ConfigurationName,
-                User = tempAccount
-            };
-            scc.PostOrdered(temp, Request);
-        }
-    }
+    
     [Route("account")]
     public class AccountController : AController<Account, int>
     {
