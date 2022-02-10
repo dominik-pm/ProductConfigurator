4d75586064b3585efa7ebd467dfc9c64736f1de9
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-10 09:09:00 +0100

    Added DescribedIndex

--- backend/BackendProductConfigurator/Model/Indexes/DescribedIndex.cs
+++ backend/BackendProductConfigurator/Model/Indexes/DescribedIndex.cs
@@ -1,14 +1,13 @@
-﻿using Model.Indexes;
-using Model.Interfaces;
+﻿using Model.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model.Languages
+namespace Model.Indexes
 {
-    public class OptionGroupVerbal : NamedIndex, IDescribable
+    public class DescribedIndex : NamedIndex, IDescribable
     {
         public string Description { get; set; }
     }
