79bb9d3291223563ca6281e1beee64f1ce86fe17
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-10 07:23:50 +0100

    Added Name, Description and Options

--- backend/BackendProductConfigurator/Model/LanguageVariant.cs
+++ backend/BackendProductConfigurator/Model/LanguageVariant.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public class LanguageVariant
+    public class LanguageVariant : INameable, IDescribable
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Option> Options { get; set; }
     }
 }
