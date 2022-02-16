c4f4f526807b255d98682702d91a9ff1aefdcb27
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-16 08:07:15 +0100

    Added logic for address getting

--- backend/BackendProductConfigurator/BackendProductConfigurator/App_Code/GlobalValues.cs
+++ backend/BackendProductConfigurator/BackendProductConfigurator/App_Code/GlobalValues.cs
@@ -1,11 +1,21 @@
 ﻿using Model.Enumerators;
+using System.Text;
 
 namespace BackendProductConfigurator.App_Code
 {
     public static class GlobalValues
     {
         public static EValueMode ValueMode { get; set; }
-        public static string ServerAddress { get; set; }
+        public static string ServerAddress
+        {
+            get
+            {
+                return new StringBuilder(serverAddress).Append(':').Append((Secure) ? Ports[0] : Ports[1]).ToString();
+            }
+            set { serverAddress = value; }
+        }
+        private static string serverAddress;
         public static bool Secure { get; set; }
+        public static int[] Ports { get; set; }
     }
 }
