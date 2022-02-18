bc3ef4304b03f692cb5181fcaef02341bb120ad5
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-18 10:31:31 +0100

    Added controller to get all image names from images folder

---
+++ backend/BackendProductConfigurator/BackendProductConfigurator/Controllers/ImagesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProductConfigurator.Controllers
+{
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [Route("/images")]
+        [HttpGet]
+        public ActionResult<List<string>> GetImages()
+        {
+            try
+            {
+                List<string> images = Directory.GetFiles("../images", "*.jpg").ToList();
+
+                Response.Headers.AcceptLanguage = Request.Headers.AcceptLanguage;
+                return images;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
