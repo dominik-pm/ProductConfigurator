bc4698378f13b6c4686673506a08231a7ed4d56a
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-21 15:54:18 +0100

    Upload

--- backend/BackendProductConfigurator/BackendProductConfigurator/Program.cs
+++ backend/BackendProductConfigurator/BackendProductConfigurator/Program.cs
@@ -7,11 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.EnableEndpointRouting = false);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 
 var MyAllowSpecificOrigins = "MyAllowSpecificOrigins";
 builder.Services.AddCors(options =>
@@ -56,23 +55,20 @@ GlobalValues.MinutesBetweenFetches = builder.Configuration.GetValue<int>("Minute
 //    );
 //}
 
-app.UseFileServer(
-        new FileServerOptions
-        {
-            FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
-            RequestPath = ""
-        }
-    );
+//app.UseFileServer(
+//        new FileServerOptions
+//        {
+//            FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+//            RequestPath = "/{*url}"
+//        }
+//    );
 
-app.UseMvc(routes =>
-{
-    routes.MapRoute(
-        name: "default",
-        template: "{controller=Home}/{action=Index}/{id?}");
-});
+
 
 app.UseAuthorization();
 app.UseAuthentication();
 app.MapControllers();
+
+app.UseStaticFiles();
 
 app.Run();
