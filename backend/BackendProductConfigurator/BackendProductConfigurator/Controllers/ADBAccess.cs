5cc9df39b61a283ea07f6b59491f013ac7734c41
t-scherzer <54886164+t-scherzer@users.noreply.github.com>
2022-02-27 15:10:30 +0100

    Added class-diagramms and error handled DB-connection

--- backend/BackendProductConfigurator/BackendProductConfigurator/Controllers/ADBAccess.cs
+++ backend/BackendProductConfigurator/BackendProductConfigurator/Controllers/ADBAccess.cs
@@ -9,67 +9,102 @@ namespace BackendProductConfigurator.Controllers
     {
         public static async Task<List<T>> GetValues(string language, string address, string api)
         {
-            HttpClient Http = GenerateHttpClient(language);
+            try
+            {
+                HttpClient Http = GenerateHttpClient(language);
 
-            return await Http.GetFromJsonAsync<List<T>>($"{address}{api}");
+                return await Http.GetFromJsonAsync<List<T>>($"{address}{api}");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
         public static async Task PutValue(string language, string address, string api, T value)
         {
-            HttpClient Http = GenerateHttpClient(language);
-
-            if ((int)Http.PutAsJsonAsync($"{address}{api}", value).Result.StatusCode != 200)
+            try
             {
-                throw new Exception("Put failed");
+                HttpClient Http = GenerateHttpClient(language);
+
+                if ((int)Http.PutAsJsonAsync($"{address}{api}", value).Result.StatusCode != 200)
+                {
+                    throw new Exception("Put failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
         public static async void PostValue(string language, string address, string api, T value)
         {
-            HttpClient Http = GenerateHttpClient(language);
-
-            if ((int)Http.PostAsJsonAsync($"{address}{api}", value).Result.StatusCode != 200)
+            try
             {
-                throw new Exception("Post failed");
+                HttpClient Http = GenerateHttpClient(language);
+
+                if ((int)Http.PostAsJsonAsync($"{address}{api}", value).Result.StatusCode != 200)
+                {
+                    throw new Exception("Post failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
         public static async Task DeleteValue(string language, string address, string api, T identifier)
         {
-            HttpClient Http = GenerateHttpClient(language);
+            try
+            {
+                HttpClient Http = GenerateHttpClient(language);
 
-            StringBuilder sb = new StringBuilder(address).Append(api).Append('/');
-            if(typeof(T) == typeof(SavedConfigWrapper))
-            {
-                sb.Append((identifier as SavedConfigWrapper).ConfigId).Append('/').Append((identifier as SavedConfigWrapper).SavedName);
-            }
-            else if(typeof(T) == typeof(ConfigurationDeleteWrapper) || typeof(T) == typeof(ConfigurationDeleteWrapper))
-            {
-                sb.Append((identifier as ConfigurationDeleteWrapper).ConfigId);
-            }
+                StringBuilder sb = new StringBuilder(address).Append(api).Append('/');
+                if (typeof(T) == typeof(SavedConfigWrapper))
+                {
+                    sb.Append((identifier as SavedConfigWrapper).ConfigId).Append('/').Append((identifier as SavedConfigWrapper).SavedName);
+                }
+                else if (typeof(T) == typeof(ConfigurationDeleteWrapper) || typeof(T) == typeof(ConfigurationDeleteWrapper))
+                {
+                    sb.Append((identifier as ConfigurationDeleteWrapper).ConfigId);
+                }
 
-            int code = (int)Http.DeleteAsync(sb.ToString()).Result.StatusCode;
-            if (code != 200)
+                int code = (int)Http.DeleteAsync(sb.ToString()).Result.StatusCode;
+                if (code != 200)
+                {
+                    throw new Exception("Deletion failed");
+                }
+            }
+            catch (Exception ex)
             {
-                throw new Exception("Deletion failed");
+                throw ex;
             }
         }
         private static HttpClient GenerateHttpClient(string language)
         {
-            HttpClientHandler handler = new HttpClientHandler();
-
-            if(!GlobalValues.Secure)
+            try
             {
-                handler.ClientCertificateOptions = ClientCertificateOption.Manual;
-                handler.ServerCertificateCustomValidationCallback =
-                    (httpRequestMessage, cert, cetChain, policyErrors) =>
-                    {
-                        return true;
-                    };
+                HttpClientHandler handler = new HttpClientHandler();
+
+                if (!GlobalValues.Secure)
+                {
+                    handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+                    handler.ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) =>
+                        {
+                            return true;
+                        };
+                }
+
+                HttpClient Http = new HttpClient(handler);
+
+                Http.DefaultRequestHeaders.Add("Accept-Language", language);
+
+                return Http;
             }
-
-            HttpClient Http = new HttpClient(handler);
-
-            Http.DefaultRequestHeaders.Add("Accept-Language", language);
-
-            return Http;
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
