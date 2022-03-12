a0e01be4f1a05701b17b72630279a42f6d74b9a1
Sleutgeb <s.leutgeb@htlkrems.at>
2022-03-12 14:19:20 +0100

    fixing loggin

--- db-service/DatabaseServiceProductConfigurator/Context/ConfiguratorContext.cs
+++ db-service/DatabaseServiceProductConfigurator/Context/ConfiguratorContext.cs
@@ -32,11 +32,6 @@ namespace DatabaseServiceProductConfigurator.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;database=product_configurator;port=3306;user=insy;password=insy", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.27-mysql"));
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
